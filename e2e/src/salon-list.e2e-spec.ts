5e62d88f215fc5746786de763619414fd0fd228c
Richard Bartelmes <richard.bartelmes@zuehlke.com>
2018-10-21 15:32:56 +0200

    create branch for solutions of e2e todos

--- e2e/src/salon-list.e2e-spec.ts
+++ e2e/src/salon-list.e2e-spec.ts
@@ -18,31 +18,29 @@ describe('Salon list', () => {
     await page.clickSalonAt(0);
     expect(await page.isSalonSelectedAt(0)).toBeTruthy();
 
-    // TODO: Click on third salon and verify that:
-    // - third salon is now highlighted
-    // - first salon is not highlighted anymore
+    await page.clickSalonAt(2);
+    expect(await page.isSalonSelectedAt(2)).toBeTruthy();
+    expect(await page.isSalonSelectedAt(0)).toBeFalsy();
   });
 
   it('should show details of clicked salon', async () => {
     await page.clickSalonAt(0);
     expect(await page.getDetailsInfoOfSalonAt(0)).toContain('Owner: Thomas Meier');
-
-    // TODO: Verify that address of selected salon contains "Bahnhofstrasse 43a"
+    expect(await page.getDetailsAddressOfSalonAt(0)).toContain('Bahnhofstrasse 43a');
   });
 
   it('should filter salons by name', async () => {
     await page.enterSearchText('Schere');
     await page.clickSearchButton();
-
-    // TODO: Verify that only one salon is shown and that its name matches "Rostige Schere"
+    expect(await page.getNumberOfSalons()).toBe(1);
+    expect(await page.getNameOfSalonAt(0)).toBe('Rostige Schere');
   });
 
   it('should filter salons by gender', async () => {
-    // TODO: Search for salons that serve gender "female" and verify that:
-    // - Result lists two salons
-    // - Served genders of the two listed salons are "both" and "female"
-    //
-    // Hint: Try to select gender in search form by value ("female") instead of element-index or id/class
-    // Use element.getAttribute('value') or css selector [value="${value}"]
+    await page.selectSearchGender('female');
+    await page.clickSearchButton();
+    expect(await page.getNumberOfSalons()).toBe(2);
+    expect(await page.getServedGenderOfSalonAt(0)).toBe('both');
+    expect(await page.getServedGenderOfSalonAt(1)).toBe('female');
   });
 });
--- e2e/src/salon-list.po.ts
+++ e2e/src/salon-list.po.ts
@@ -2,13 +2,16 @@ import { browser, by, element } from 'protractor';
 
 export class SalonListPage {
   private searchTextInput = element(by.css('.search-form input[type="text"]'));
+  private searchGenderInputs = element.all(by.css('.search-form input[type="radio"]'));
   private searchButton = element(by.css('.search-form button'));
   private salonContainers = element.all(by.css('.salon-container'));
 
   private salonHeaderLoc = by.css('.salon-header');
   private salonHeaderNameLoc = by.css('div:nth-of-type(1)');
+  private salonHeaderGenderLoc = by.css('div:nth-of-type(2)');
   private salonDetailsLoc = by.tagName('app-salon-details');
   private detailsInfoLoc = by.css('.details-info');
+  private detailsAddressLoc = by.css('.details-address');
 
   private selectedHeaderClass = 'selected';
 
@@ -20,8 +23,20 @@ export class SalonListPage {
     return this.getSalonHeaderAt(index).element(this.salonHeaderNameLoc).getText();
   }
 
+  getNumberOfSalons() {
+    return this.salonContainers.count();
+  }
+
+  getServedGenderOfSalonAt(index: number) {
+    return this.getSalonHeaderAt(index).element(this.salonHeaderGenderLoc).getText();
+  }
+
   getDetailsInfoOfSalonAt(index: number) {
     return this.getSalonDetailsAt(index).element(this.detailsInfoLoc).getText();
+  }
+
+  getDetailsAddressOfSalonAt(index: number) {
+    return this.getSalonDetailsAt(index).element(this.detailsAddressLoc).getText();
   }
 
   async isSalonSelectedAt(index: number) {
@@ -35,6 +50,13 @@ export class SalonListPage {
 
   async clickSearchButton() {
     await this.searchButton.click();
+  }
+
+  async selectSearchGender(value: string) {
+    const elementsWithValue = this.searchGenderInputs.filter(async (el) => await el.getAttribute('value') === value);
+    await elementsWithValue.first().click();
+
+    // With css selector: await element(by.css(`.search-form input[type="radio"][value="${value}"]`)).click();
   }
 
   async enterSearchText(text: string) {
